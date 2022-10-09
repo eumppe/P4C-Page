94d1743ed0808379b6930d488b9a57866536672c
eumppe <pych05@unist.ac.kr>
2022-10-10 02:28:39 +0900

    please-load-css

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Eumppe</title>
-		<link rel="stylesheet" href="./css/main.css">
+		<link rel="stylesheet" href="./css/main.css" type="text/css">
 	</head>
 	<body>
 			<header>
